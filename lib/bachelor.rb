247bb6263c34d4ef123ff3694c256d7637ad4d29
Anthony Chang <awchang12@gmail.com>
2018-05-09 22:02:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,8 @@ def get_first_name_of_season_winner(data, season)
   # code here
   winner = ""
   data[season].each do |contestant|
-    if contestant["status"] == "winner"
+    if contestant["status"] == "Winner"
+      winner.push
 end
 
 def get_contestant_name(data, occupation)
