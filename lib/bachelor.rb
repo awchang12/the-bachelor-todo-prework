13220d554f94a4d6da90d4d7c286e3cfa4d6321f
Anthony Chang <awchang12@gmail.com>
2018-05-09 22:10:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -24,6 +24,15 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  contestants_from_hometown = []
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        contestants_from_hometown.push(contestants_hash["name"])
+      end
+    end
+  end
+  return contestants_from_hometown.length
 end
 
 def get_occupation(data, hometown)
